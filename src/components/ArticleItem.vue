8084a5a8562a537d26c29b02e5600100410a923f
hhartz <henri.hartz@gmail.com>
2026-09-20 22:21:15 +0200

    feat: article item

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    id: Number,
+    name: String,
+    description: String,
+    price: Number 
+})
+
+function handleClick() {
+  this.$router.push(`/articles/${props.id}`)
+}
+
+</script>
+<template>
+    <div class="item-card" :id="id">
+        <h2>{{ name }}</h2>
+        <p class="description">{{ description }}</p>
+        <p class="price">{{ price }} €</p>
+        <button @click="handleClick">Voir plus</button>
+    </div>
+</template>
